622ac43665819252a784fcb152fd4a5879f1157d
gianpaoloramos <gianramos1023@gmail.com>
2022-10-13 17:19:35 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.net.URI;
+
+public class SearchEngine {
+    public String handleRequest(URI url) {
+        if(url.getPath().equals("/")) {
+            
+        }
+    }
+}
